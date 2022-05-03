b3e04fea807101a2358a13fb645483f1db078c06
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-05-03 16:32:14 +0200

    lots of keys

--- wp-config.php
+++ wp-config.php
@@ -1,5 +1,13 @@
 <?php 
 
+define('AUTH_KEY',         '-DHo*$P0@q@lG-Qb+9(VzR7a,PL-&r5*oOiclW`9xQ5=y.Uvg*cQCx^ jKkwEF=D');
+define('SECURE_AUTH_KEY',  'G$smyv3#c[]#F/Q<$K%[,>s84e9+fw*_0!=G>srz3[]wFR|Zm&7Db9~&<Xvl$,m%');
+define('LOGGED_IN_KEY',    '@Ff@w>y=m-8N;{%aK.%$J+X9_=mHd[e,6Qjw~3VQxL8QpF0Nv|%86F8l$z;6K]@N');
+define('NONCE_KEY',        'Hv.k6iN;Ef[fF9$4%},8Vt+SXeCp_:%k3R6?~24)|F@vA|..x-3q^^TUqkG[yXC+');
+define('AUTH_SALT',        'yE}K)7@:Ab]u;+ DQ81-%D+++lD;NT/hd2WSW?Jz9BdE>||. HsfszZ-Vg7lUt4I');
+define('SECURE_AUTH_SALT', '0>]vN?[fq_f.Qgah|nZC+qGVt gM-|q9P[TtYbpPdDQ X}b^S2R1]x<#<r{$(Oo~');
+define('LOGGED_IN_SALT',   'A{Iu2PpVYV9($_GlGA-}-ork}?QPRqqDWdCtA7PBD`QuJ3#B4mf-Hmf.t]23{k Y');
+define('NONCE_SALT',       'pGZV8}]J)KR7M lujg&|8fu7QpGY0Fv`H{pP|IX-5,qA9{3u-QdcMK`*B0R|7. 0');
 define('JWT_AUTH_SECRET_KEY', 'Xb|x_Ik{x^,hgV=iJZ)B_PIw[8- 7=AP7g]$gQw[uRIDv;&e |5Cm/B3)j[3!#)T');
 
 define('JWT_AUTH_CORS_ENABLE', true);
