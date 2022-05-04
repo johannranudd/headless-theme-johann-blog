1ee5bb74eafeef0e350eb7357ba922e822c508b0
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-05-04 12:55:32 +0200

    removing wp-config-php and .htaccess

--- index.php
+++ index.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- <link rel="stylesheet" href="./style.css" /> -->
     <title>Headless-CMS</title>
-    
   </head>
-  <body>
-  
-  </body>
+  <body></body>
 </html>
